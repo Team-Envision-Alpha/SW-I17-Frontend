34afac1f2351de90d3306682b156d05c2b155cff
Abhishek Dubey <7abhishek1410@gmial.com>
2022-03-12 14:59:58 +0530

    login ui completed

---
+++ src/Components/Login.js
@@ -0,0 +1,56 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <>
+      <div className="w-full max-w-xs h-full mx-auto py-[25vh]">
+        <form className="bg-white shadow-md rounded flex flex-col px-8 pt-6 pb-8 mb-4 gap-4 ">
+          <div className="flex flex-col gap-2 justify-center">
+            <label
+              className="block text-gray-700 text-sm font-bold "
+              htmlFor="username"
+            >
+              Username
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="username"
+              type="text"
+              placeholder="Username"
+            />
+          </div>
+          <div className="flex flex-col gap-2 justify-center">
+            <label
+              className="block text-gray-700 text-sm font-bold "
+              htmlFor="password"
+            >
+              Password
+            </label>
+            <input
+              className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              id="password"
+              type="password"
+              placeholder="******************"
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="button"
+            >
+              Sign In
+            </button>
+            <a
+              className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+              href="#"
+            >
+              Forgot Password?
+            </a>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default Login;
