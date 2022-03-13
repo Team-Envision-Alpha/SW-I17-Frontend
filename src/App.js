d38769ec2390ead4f0b8a62aca683b987125791b
Rishit Shivesh <rishitshivesh@gmail.com>
2022-03-13 16:43:31 +0530

    FB Login

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from "react";
 import Login from "./Components/Login";
-
+import { 
+  Routes, 
+  Route, 
+  BrowserRouter as Router
+ } from "react-router-dom";
+import FB from "./Components/FB"
 const App = () => {
   return (
     <>
-      <Login />
+    <Router>
+      <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/fb" element={<FB/>}/>
+      </Routes>
+    </Router>
     </>
   );
 };
