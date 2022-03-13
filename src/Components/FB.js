81f430f3a5d49dfdba2633573b7eb0f9fff19d87
DivyanshuKaushik <divyanshukaushik44@gmail.com>
2022-03-13 17:12:31 +0530

    fb integration

--- src/Components/FB.js
+++ src/Components/FB.js
@@ -1,66 +1,22 @@
-import React,{
-    useState,
-    useEffect,
-} from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Navigate } from "react-router-dom";
 
 import FacebookLogin from "react-facebook-login";
 
-export default function FB(){
-    const fbuserid = localStorage.getItem("fbuserid");
-    const fbaccesstoken = localStorage.getItem("fbaccesstoken");
-    const fbpageid = localStorage.getItem("fbpageid");
-    const [update, setupdate] = useState(false);
-    const history = Navigate();
-  useEffect(() => {
-    axios
-      .get(
-        `https://graph.facebook.com/${fbuserid}/accounts?fields=name,access_token&access_token=${fbaccesstoken}`
-      )
-      .then((response) => {
-        if (Array.isArray(response.data.data) && response.data.data.length) {
-          // Setuseraccesstoken(response.data.data[0].access_token);
-          console.log(response);
-          localStorage.setItem(
-            "fbaccesstoken",
-            response.data.data[0].access_token
-          );
-          localStorage.setItem("fbpageid", response.data.data[0].id);
-        } else {
-          alert("No page found linked to this account");
-          window.location.reload();
-        }
-      });   
-  }, [update]);
-
-  
-
-    return(
-        <div>
-            <h1 className="text-center">FB</h1>
-            <FacebookLogin
-      appId="647567206327849"
-      autoLoad={false}
-      fields="name,email,picture"
-      // onClick={componentClicked}
-      scope="pages_show_list,read_page_mailboxes,pages_messaging,pages_read_engagement, pages_manage_metadata,pages_manage_posts,pages_read_engagement, public_profile"
-      callback={(response) => {
-        console.log(response);
-        // setUserdata(response);
-        // Setuseraccesstoken(response.accessToken);
-        // setUseruid(response.userID);
-        localStorage.setItem("fbaccesstoken", response.accessToken);
-        localStorage.setItem("fbuserid", response.userID);
-        setupdate(!update);
-        if (response.userID) {
-          history.push("/");
-          // history.push("/facebook");
-          history.location.pathname.replace("/facebook");
-        }
-        // window.location.reload();
-      }}
-    />
-        </div>
-    )
+export default function FB() {
+  return (
+    <div>
+      <h1 className="text-center">FB</h1>
+      <FacebookLogin
+        appId="647567206327849"
+        autoLoad={false}
+        fields="name,email,picture"
+        scope="pages_show_list,read_page_mailboxes,pages_messaging,pages_read_engagement, pages_manage_metadata,pages_manage_posts,pages_read_engagement, public_profile"
+        callback={(response)=>{
+          console.log(response)
+        }}
+      />
+    </div>
+  );
 }
