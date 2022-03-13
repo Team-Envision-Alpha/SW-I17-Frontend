c32a7e984aacbbb41cd002e3ddf7d23aed6b717c
DivyanshuKaushik <divyanshukaushik44@gmail.com>
2022-03-13 15:31:51 +0530

    integrated backend and apollo client setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Login from "./Components/Login";
+import { gql,useQuery} from "@apollo/client";
 
 const App = () => {
+  const HELLO = gql`
+    query{
+      hello
+    }
+  `
+  const {loading,error,data} = useQuery(HELLO)
+  console.log(data)
   return (
     <>
       <Login />
