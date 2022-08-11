206ff8eb193956614c13908b3f5724d9bf165d7f
shrishti300 <sg2195@srmist.edu.in>
2022-08-11 19:01:40 +0530

    frontent changes on events and login page

--- src/pages/EditEvent.js
+++ src/pages/EditEvent.js
@@ -12,23 +12,24 @@ const DashBoard = () => {
         <div className="flex flex-row mt-5">
           <p className="px-10">Invited Events</p>
           <p className="px-10 font-bold decoration -[3px]">
-            <span className="underline decoration-2 ...">Upc</span>oming Events
+            <span className="underline decoration-2 ...">Upc</span>Coming Events
           </p>
         </div>
-        <div className="container lg:px-20 mx-auto pb-20">
-          <div className="flex justify-center items-center grid md:grid-cols-2 sm:grid-cols-1">
+        
+          <div className="flex flex-col container lg:px-20 mx-auto pb-20">
+          <div className="flex-row justify-center items-center grid md:grid-cols-2 sm:grid-cols-1">
             <EventCard name="Edit"></EventCard>
             <EventCard name="Edit"></EventCard>
+            <EventCard name="Edit"></EventCard>
+
           </div>
-          <div className="flex justify-center items-center grid md:grid-cols-2 sm:grid-cols-1">
+          <div className="flex-row justify-center items-center grid md:grid-cols-2 sm:grid-cols-1">
             <EventCard name="Edit"></EventCard>
-            <EventCard name="Edit"></EventCard>
-          </div>
-          <div className="flex justify-center items-center grid md:grid-cols-2 sm:grid-cols-1">
             <EventCard name="Edit"></EventCard>
             <EventCard name="Edit"></EventCard>
           </div>
         </div>
+        
       </div>
     </>
   );
