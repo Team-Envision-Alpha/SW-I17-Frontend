1cabb1bc78215b6759333c512bcd0e46445e548a
shrishti300 <88239226+shrishti300@users.noreply.github.com>
2022-08-25 17:47:29 +0530

    ui update

--- src/Components/Temp.js
+++ src/Components/Temp.js
@@ -8,6 +8,7 @@ return (
                <div className="w-[80w] md:w-[50vw] bg-[#f3b641] shadow-xl rounded-2xl overflow-y-hidden my-10 mx-auto lg:mx-[5vw]">
                <div className="w-full h-full py-5 mt-5 bg-white px-10 pb-20 justify-center ">
                <div className="flex flex-col gap-4">
+               <p className="text-center text-xl font-bold"> RECENT ACTIVITY </p>
              <Tempcomponent />
              <Tempcomponent />
              <Tempcomponent />
