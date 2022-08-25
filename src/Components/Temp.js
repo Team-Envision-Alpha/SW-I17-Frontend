339436625c067c559b86e7b9ab9df1b2aa957bb8
shrishti300 <88239226+shrishti300@users.noreply.github.com>
2022-08-25 17:54:55 +0530

    Update Temp.js

--- src/Components/Temp.js
+++ src/Components/Temp.js
@@ -3,9 +3,9 @@ import Tempcomponent from "../Components/Tempcomponents";
 const Temp = () => {
 return (
     <>
-               <div className="flex flex-col gap-10 font-IBM-Sans px-8 py-10">
+               <div className="flex flex-col justify-center items-center gap-10 font-IBM-Sans px-8 py-10">
                
-               <div className="w-[80w] md:w-[50vw] bg-[#f3b641] shadow-xl rounded-2xl overflow-y-hidden my-10 mx-auto lg:mx-[5vw]">
+               <div className="justify-center bg-[#f3b641] shadow-xl rounded-2xl overflow-y-hidden my-10 mx-auto lg:mx-[5vw]">
                <div className="w-full h-full py-5 mt-5 bg-white px-10 pb-20 justify-center ">
                <div className="flex flex-col gap-4">
                <p className="text-center text-xl font-bold"> RECENT ACTIVITY </p>
