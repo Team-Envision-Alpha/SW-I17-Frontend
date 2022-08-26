80b7d23ddf57fcf121e1053d779197cc8e6bf792
Abhishek Dubey <7abhishek1410@gmail.com>
2022-08-26 16:48:36 +0530

    welcome-screen -added

--- src/pages/Tourpage.js
+++ src/pages/Tourpage.js
@@ -1,35 +1,29 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import logo from "../Assets/Images/logo.svg";
 import Tourcarousel from "../Components/Tourcarousel";
-const TourPage= () => {
-
-    return (
-        <>
-        <div className="flex flex-col gap-4 items-center justify-center">
-           <div>
-            <img src={logo} alt="logo" className="w-[32vw] h-[15vh] mt-16"  />
-           </div>
-           <div>
-           <Tourcarousel />
-           </div>
-           <div className="flex items-center gap-16">
-<div>
-<button class="float-left px-16 ml-10 bg-white text-green-700 border-green-700 border-2 font-bold p-1 mt-2 rounded-lg h">
-                      skip
-                    </button>
-                    </div>
-                    <div>
-                   <button
-                      class="hover:bg-green-700 px-16 text-white font-bold p-1 pb-2 mb-2 mt-2 rounded-lg h"
-                      style={{ backgroundColor: "#1F8B24" }}
-                    >
-                      next
-                    </button>
-                    </div>
-</div>
-
+import {Link} from "react-router-dom"
+const TourPage = () => {
+  
+  return (
+    <>
+      <div className="h-[100vh] flex flex-col gap-6 items-center justify-center px-16">
+        <div>
+          <img src={logo} alt="logo" className="w-[32vw] h-[15vh] " />
         </div>
-        </>
-    );
+        <div>
+          <Tourcarousel />
+        </div>
+   
+          <Link to="/login">
+            <button className="float-left px-16 ml-10 bg-white text-green-700 border-green-700 border-2 font-bold p-1 mt-2 rounded-lg hover:bg-green-700 hover:text-white">
+              Log In
+              </button>
+            </Link>
+       
+      
+        </div>
+      
+    </>
+  );
 };
 export default TourPage;
--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,54 +1,78 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import logo from "../Assets/Images/logo.svg";
 import hands from "../Assets/Images/hands.svg";
-import {useNavigate} from "react-router-dom";
+import TourPage from "./Tourpage";
+import { Link } from "react-router-dom";
 
 const Welcome = () => {
-    const navigate = useNavigate()
-    return (
-        <>
-     <div className="flex flex-col gap-4 items-center justify-center">
-     <div>
-        <img src={logo} alt="logo" className="w-[32vw] h-[15vh] mt-16" />
-     </div>
-     <div>
-        <p className="font-bold text-[50px]"> Welcome To The Portal</p>
-     </div>
-<div>
-    <img src={hands} alt="namaste"/>
-</div>
-<div className="text-center">
-<p className="font-bold text-xl text-sky-600 ml-32 mr-32 text-wrap"> Our Objective is to <span className="text-orange-400">Promote Quality </span>in
-Technical Education,<span className="text-orange-400"> Plan and Coordinate</span> Development
-of Technical Education System,<span className="text-orange-400"> Regulate and Maintain</span> the
-Norms and Standards.</p>
-</div>
-<div>
-<a href="/" className="text-sky-600 font-bold"> Have a look around in the portal</a>
-</div>
-<div className="flex items-center gap-16">
-<div>
-<button class="float-left px-8 ml-10 bg-white text-red-600 border-red-600 border-2 font-bold p-1 mt-2 rounded-lg h"
-onClick={()=>navigate("/login")}>
-                      Log In
-                    </button>
-                    </div>
-                    <div>
-
-                   <button
-                      class="hover:bg-green-700 px-8 text-white font-bold p-1 pb-2 mb-2 mt-2 rounded-lg h"
-                      style={{ backgroundColor: "#1F8B24" }}
-                      onClick={()=>navigate("/tour")}
-                    >
-                      Next
-                    </button>
-                    </div>
-</div>
-
-</div>
-       
-        </>
-    )
+ 
+  
+  
+  return (
+    <>
+      <div className="h-[100vh] flex justify-center items-center">
+        {show ? (
+          <>
+ 
+            <div className="flex flex-col gap-4 items-center justify-center">
+              <div>
+                <img src={logo} alt="logo" className="w-[32vw] h-[15vh] " />
+              </div>
+              <div>
+                <p className="font-bold text-[50px]">
+                  {" "}
+                  Welcome To The AICTE Portal
+                </p>
+              </div>
+              <div>
+                <img src={hands} alt="namaste" />
+              </div>
+              <div className="text-center">
+                <p className="font-bold text-xl text-sky-600 mx-32 text-wrap">
+                  {" "}
+                  Our Objective is to{" "}
+                  <span className="text-orange-400">Promote Quality </span>in
+                  Technical Education,
+                  <span className="text-orange-400"> Plan and Coordinate</span>{" "}
+                  Development of Technical Education System,
+                  <span className="text-orange-400">
+                    {" "}
+                    Regulate and Maintain
+                  </span>{" "}
+                  the Norms and Standards.
+                </p>
+              </div>
+              <div>
+                <p className="text-sky-600 font-bold text-lg">
+                  {" "}
+                  Have a look around in the portal!
+                </p>
+              </div>
+              <div className="flex items-center justify-center gap-16">
+                <Link to="/login">
+                  <button class="p-2 w-32 bg-white text-red-600 border-red-600 hover:text-white hover:bg-red-600 border-2 font-bold rounded-lg h">
+                    Log In
+                  </button>
+                </Link>
+                <div>
+                  <button
+                    class="bg-green-700 p-2 w-32 text-white hover:bg-white hover:text-green-700 hover:border-2 hover:border-green-700 font-bold rounded-lg h"
+                    onClick={() => setShow(false)}
+                  >
+                    Next
+                  </button>
+                </div>
+              </div>
+            </div>
+          </>
+        ) : (
+          <>
+            <TourPage />
+          </>
+        )}
+      </div>
+    </>
+  );
 };
 
 export default Welcome;
